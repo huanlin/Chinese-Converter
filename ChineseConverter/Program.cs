a90266880ba111206329e0747eefc9dfa62d7a36
Michael Tsai <huanlin.tsai@gmail.com>
2014-10-06 03:46:55 +0800

    Initial version. Supported file conversion and custom dictionary.

---
+++ ChineseConverter/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChineseConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ChineseConverter v0.1 by Huan-Lin Tsai (2014)");
+
+            if (args.Length < 3)
+            {
+                ShowUsage();
+                return;
+            }
+
+            string srcFileName = args[0];
+            string dstFileName = args[1];
+
+            string direction = args[2];
+            string dirLabel = null;
+            var convDirection = TSChineseConverterDirection.SimplifiedToTraditional;
+            if ("t2s".Equals(direction, StringComparison.InvariantCultureIgnoreCase))
+            {
+                convDirection = TSChineseConverterDirection.TraditionalToSimplified;
+                dirLabel = "繁->簡";
+            }
+            else if ("s2t".Equals(direction, StringComparison.InvariantCultureIgnoreCase)) 
+            {
+                convDirection = TSChineseConverterDirection.SimplifiedToTraditional;
+                dirLabel = "簡->繁";
+            }
+            else 
+            {
+                Console.WriteLine("Invalid conversion direction: " + direction);
+                Console.WriteLine("Use 't2s' or 's2t'.");
+                return;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("輸入檔案: {0}", srcFileName);
+            Console.WriteLine("輸出檔案: {0}", dstFileName);
+            Console.WriteLine("轉換操作: {0}", dirLabel);
+
+            Dictionary<string, string> dict = null;
+            if (args.Length >= 4)
+            {
+                string dictFileName = args[3];
+                if (!File.Exists(dictFileName))
+                {
+                    Console.WriteLine("檔案不存在: " + dictFileName);
+                    return;
+                }
+                dict = LoadDictionary(dictFileName);
+                Console.WriteLine("使用字典: {0}", dictFileName);
+            }                
+
+            // 執行轉換
+            Console.WriteLine("正在轉換.....");
+
+            var converter = new TSChineseConverter();
+            try 
+            {
+                converter.Convert(srcFileName, dstFileName, convDirection, dict);
+                Console.WriteLine();
+                Console.WriteLine("轉換完畢!");
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine("轉換時發生錯誤: ");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static Dictionary<string, string> LoadDictionary(string dictFileName)
+        {
+            var dict = new Dictionary<string, string>();
+
+            using (var reader = new StreamReader(dictFileName))
+            {
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    if (!String.IsNullOrWhiteSpace(line) && !line.StartsWith(";") && line.IndexOf('=') > 0)
+                    {
+                        var words = line.Split('=');
+                        if (words.Length == 2)
+                        {
+                            dict.Add(words[0], words[1]);
+                        }
+                    }
+                    line = reader.ReadLine();
+                }
+            }
+
+            return dict;
+        }
+
+        static void ShowUsage() 
+        {
+            Console.WriteLine("Usage:");
+            Console.WriteLine("ChineseConverter <InputFile> <OutputFile> <ConversionDirection> [DictionaryFile]");
+            Console.WriteLine();
+            Console.WriteLine("Arguments:");
+            Console.WriteLine("ConversionDirection - t2s or s2t.");
+        }
+    }
+}
---
+++ ChineseConverter/TSChineseConverter.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.IO;
+using Microsoft.Office.Interop.Word;
+
+namespace ChineseConverter
+{
+    public enum TSChineseConverterDirection
+    {
+        TraditionalToSimplified,
+        SimplifiedToTraditional
+    };
+
+    /// <summary>
+    /// 修改自「黑暗執行緒」的文章：http://blog.darkthread.net/post-2013-08-15-office-tcscconverter.aspx
+    /// </summary>
+    public class TSChineseConverter : IDisposable
+    {
+        private Application wordApp = null;
+        private Document doc = null;
+
+        public TSChineseConverter()
+        {
+            wordApp = new Application();
+            wordApp.Visible = false;
+            doc = wordApp.Documents.Add();
+        }
+
+        public string Convert(string src, TSChineseConverterDirection direction)
+        {
+            string result = null;
+            var tcscDirection = WdTCSCConverterDirection.wdTCSCConverterDirectionSCTC;
+            if (direction == TSChineseConverterDirection.TraditionalToSimplified)
+            {
+                tcscDirection = WdTCSCConverterDirection.wdTCSCConverterDirectionTCSC;
+            }
+            doc.Content.Text = src;
+            doc.Content.TCSCConverter(tcscDirection, true, true);
+            //取回轉換結果時，結尾會多出\r
+            result = doc.Content.Text.TrimEnd('\r');
+            return result;
+        }
+
+        /// <summary>
+        /// 轉換整個檔案。
+        /// </summary>
+        /// <param name="srcFileName">來源檔名。</param>
+        /// <param name="dstFileName">目的檔名。</param>
+        /// <param name="direction">轉換方向。</param>
+        /// <param name="customDictionary">自訂詞彙轉換表。注意：在呼叫 WORD 轉換之前會先使用此表進行字串替換。</param>
+        public void Convert(string srcFileName, string dstFileName, TSChineseConverterDirection direction, 
+            Dictionary<string, string> customDictionary = null)
+        {
+            if (!File.Exists(srcFileName))
+            {
+                throw new FileNotFoundException("指定的來源檔案不存在: " + srcFileName);
+            }
+
+            using (var dstFileWriter = new StreamWriter(dstFileName))
+            {
+                using (var srcFileReader = new StreamReader(srcFileName))
+                {
+                    string line = srcFileReader.ReadLine();
+                    while (line != null)
+                    {
+                        if (customDictionary != null)
+                        {
+                            line = ApplyCustomDictionary(line, customDictionary);
+                        }
+
+                        string convertedLine = Convert(line, direction);
+                        dstFileWriter.WriteLine(convertedLine);
+
+                        line = srcFileReader.ReadLine();
+                    }
+                    srcFileReader.Close();
+                    dstFileWriter.Close();
+                }
+            }            
+        }
+
+        private string ApplyCustomDictionary(string input, Dictionary<string, string> customDictionary)
+        {
+            StringBuilder sb = new StringBuilder(input);
+            foreach (string key in customDictionary.Keys)
+            {
+                sb.Replace(key, customDictionary[key]);
+            }
+            return sb.ToString();
+        }
+
+        public void Dispose()
+        {
+            //確實關閉Word Application
+            try
+            {
+                //關閉Word檔
+                object dontSave = WdSaveOptions.wdDoNotSaveChanges;
+                ((_Document)doc).Close(ref dontSave);
+                //確保Document COM+釋放
+                if (doc != null)
+                    Marshal.FinalReleaseComObject(doc);
+                doc = null;
+                ((_Application)wordApp).Quit(ref dontSave);
+            }
+            finally
+            {
+                Marshal.FinalReleaseComObject(wordApp);
+            }
+        }
+    }
+}
